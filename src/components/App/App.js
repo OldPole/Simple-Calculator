0810d0d6de6bb4441726b8ea7d06fff24ee2bc2e
OldPole <ipetr6828@gmail.com>
2025-11-01 22:39:08 +0300

    I created a basic project structure and added a webpack configuration.

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import styles from './App.module.css';
+
+console.log(styles);
+
+class App {
+  render() {
+    const root = document.getElementById('root');
+
+    root.innerHTML = `
+        <h1 class="${styles.header}">PIWO</h1>
+    `;
+  }
+}
+
+export default new App();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from './components/App';
+
+import './styles/main.css';
+
+(() => {
+  App.render();
+})();
