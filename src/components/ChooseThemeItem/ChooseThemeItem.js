4286eda7ed199a7e88c3cf53ddfd4941b8c733e8
OldPole <ipetr6828@gmail.com>
2025-11-05 23:43:48 +0300

    Added a visual display of the selected theme. Styles have been slightly adjusted.

--- src/components/ChooseThemeItem/ChooseThemeItem.js
+++ src/components/ChooseThemeItem/ChooseThemeItem.js
@@ -1,16 +1,32 @@
-import { setTheme } from '../../utils/localStorage';
+import { getTheme, setTheme } from '../../utils/localStorage';
 
 import styles from './ChooseThemeItem.module.css';
 
 class ChooseThemeItem {
-  handleChangeTheme = (theme) => {
+  handleChangeTheme(theme) {
     setTheme(theme);
-  };
+    this.updateActiveTheme();
+  }
+
+  updateActiveTheme() {
+    const currentTheme = getTheme();
+
+    document.querySelectorAll('[data-theme]').forEach((item) => {
+      if (item.getAttribute('data-theme') === currentTheme) {
+        item.classList.add(styles.theme__item_active);
+      } else {
+        item.classList.remove(styles.theme__item_active);
+      }
+    });
+  }
 
   render({ theme, classes }) {
+    const currentTheme = getTheme();
+    const activeClass = theme === currentTheme ? styles.theme__item_active : '';
+
     const html = `
       <div 
-        class="${styles.theme__item} ${styles[classes]}" 
+        class="${styles.theme__item} ${styles[classes]} ${activeClass}" 
         data-theme=${theme}
       >
       </div>
--- src/services/changeCssVars.js
+++ src/services/changeCssVars.js
@@ -1,7 +1,15 @@
+/*
+Формат CSS-переменной:
+--theme-default-УникальноеИмя # дефолтная переменная
+--theme-light-УникальноеИмя   # для "light"
+--theme-dark-УникальноеИмя    # для "dark"
+--theme-neitral-УникальноеИмя # для "neitral"
+*/
+
 export const changeCssVars = (theme) => {
   const root = document.querySelector(':root');
 
-  const vars = ['text', 'background', 'function', 'operator', 'number'];
+  const vars = ['text', 'background', 'function', 'operator', 'number', 'border'];
 
   vars.forEach((element) => {
     root.style.setProperty(`--theme-default-${element}`, `var(--theme-${theme}-${element})`);
